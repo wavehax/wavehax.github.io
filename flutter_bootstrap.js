dac21a15445e56a8abfc3c2e50c61525ec5993c5
Adrian <canyoudigital@gmail.com>
2026-06-28 16:38:52 -0700

    Deploy web build 2026-06-28

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,12 +38,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3373383444" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
+    serviceWorkerVersion: "1796593878" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
   },
-  onEntrypointLoaded: async function(engineInitializer) {
-    const appRunner = await engineInitializer.initializeEngine({
-      renderer: "html",
-    });
-    await appRunner.runApp();
-  }
 });
