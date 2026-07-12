5edae5b0aacf3d23f7c762c1e6ce5881a0d8fc5b
Adrian <canyoudigital@gmail.com>
2026-07-12 15:47:26 -0700

    Deploy web build 2026-07-12e

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3753080493" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
+    serviceWorkerVersion: "2321165200" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
   },
 });
