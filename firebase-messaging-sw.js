4039ac39e091fe41307be1fde2f7b55f56acc47f
Adrian <canyoudigital@gmail.com>
2026-07-12 20:55:40 -0700

    Deploy web build 2026-07-12 (web FCM: /fcm-sw/ SW + VAPID key)

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -10,12 +10,7 @@ firebase.initializeApp({
   appId: "1:568359971803:web:50fd26b2fdd97af7d815fe",
 });
 
-const messaging = firebase.messaging();
-
-messaging.onBackgroundMessage((payload) => {
-  const { title, body } = payload.notification ?? {};
-  self.registration.showNotification(title ?? "JobArray", {
-    body: body ?? "",
-    icon: "/icons/Icon-192.png",
-  });
-});
+// Instantiating messaging attaches the SDK's push handlers. The SDK
+// auto-displays messages that carry a `notification` payload (all of ours do);
+// adding an onBackgroundMessage showNotification on top would duplicate them.
+firebase.messaging();
--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1724351676" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
+    serviceWorkerVersion: "4006990960" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
   },
 });
